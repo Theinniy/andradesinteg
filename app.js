f3ef3c179cc26a5d9e0fc5463f61cd4ba9a62eb4
Theinniy <thiy.amorim@gmail.com>
2025-04-24 13:12:17 -0300

    feat: Ajuste responsividade para mobile-2

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ const menuToggle = document.getElementById('menu-toggle');
   menuToggle.addEventListener('click', () => {
     menuUl.classList.toggle('aberto');
   });
+  
+function ajustarPaddingCTA() {
+    const header = document.querySelector('header');
+    const cta = document.querySelector('.cta');
+
+    if (header && cta) {
+      const alturaHeader = header.offsetHeight;
+      cta.style.paddingTop = alturaHeader + 'px';
+    }
+  }
+
+  // Ajusta ao carregar a página
+  window.addEventListener('load', ajustarPaddingCTA);
+
+  // Ajusta também se a janela for redimensionada (por exemplo, virar o celular)
+  window.addEventListener('resize', ajustarPaddingCTA);
+
