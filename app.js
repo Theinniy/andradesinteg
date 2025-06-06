a1b34692a14f270f596105ecaafd43cd8539bf1e
Theinniy <thiy.amorim@gmail.com>
2025-06-06 15:33:31 -0300

    feat: Inserindo Submenu

--- app.js
+++ app.js
@@ -23,3 +23,21 @@ function ajustarPaddingCTA() {
   // Ajusta também se a janela for redimensionada (por exemplo, virar o celular)
   window.addEventListener('resize', ajustarPaddingCTA);
 
+  document.querySelectorAll('.menu a').forEach(link => {
+  link.addEventListener('click', (e) => {
+    const parentLi = link.parentElement;
+    const isSubmenuParent = parentLi.classList.contains('submenu');
+
+    // Se for pai de submenu, impede de fechar o menu e permite abrir o submenu
+    if (isSubmenuParent) {
+      e.preventDefault(); // evita pular para #servicos
+    } else {
+      menuUl.classList.remove('aberto');
+    }
+  });
+});
+
+
+
+  
+
